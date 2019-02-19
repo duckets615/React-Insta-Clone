2c67afa9eea9192c5c36591cdaee26b0ad0e8701
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-02-19 16:34:12 -0500

    searchbar edit

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import CommentSection from './components/CommentSection/CommentSection';
-import PostContainer from './components/PostContainer/PostContainer';
-import dummyData from './dummy-data';
+import React from 'react';
+import Navigation from './Navigation';
+import './SearchBar.css';
+import logo from './img/logo.PNG';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dummyData
-    }
-  }
-  render() {
+const SearchBar = props => {
     return (
-      <div className="App">
-       Hi Hi Buddy
-       <SearchBar />
-       <CommentSection />
-      </div>
+        <div className="search-container">
+        <div className="search-bar">
+            <div className="logo">
+                <button><img src={logo} alt="instagram"/></button>
+            </div>
+            <div className="search">
+                <input placeholder="Search" onFocus={props.focusSearch} onBlur={props.focusSearch} onChange={props.updateSearch}/>
+                <div className="search-icon" style={{transform: 'rotate(-45deg)', fontWeight:'bold'}}>&#9906;</div>
+            </div>
+
+            <Navigation />
+        </div>
+        </div>
+
     );
-  }
 }
 
-export default App;
+export default SearchBar;
