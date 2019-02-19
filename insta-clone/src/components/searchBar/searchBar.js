88cc33db029d3a50ba1032da9f4b3ae6bb5774dd
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-02-19 08:44:07 -0500

    search bar complete

---
+++ insta-clone/src/components/searchBar/searchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navigation from './Navigation';
+import './SearchBar.css';
+import logo from './img/logo.PNG';
+
+const SearchBar = props => {
+    return (
+        <div className="search-container">
+        <div className="search-bar">
+            <div className="logo">
+                <a href="#"><img src={logo} /></a>
+            </div>
+            <div className="search">
+                <input value="Search" />
+                <div class="search-icon" style={{transform: 'rotate(-45deg)', fontWeight:'bold'}}>&#9906;</div>
+            </div>
+
+            <Navigation />
+        </div>
+        </div>
+
+    );
+}
+
+export default SearchBar;
